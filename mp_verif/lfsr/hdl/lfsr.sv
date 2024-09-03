de229ad2a4f408144b708df6d663e0f5463063a8
Kevin Chen <kpchen2@illinois.edu>
2024-09-03 08:10:44 -0500

    completed part 1

--- mp_verif/lfsr/hdl/lfsr.sv
+++ mp_verif/lfsr/hdl/lfsr.sv
@@ -9,5 +9,31 @@ module lfsr #(
 );
 
     // TODO: Fill this out!
+    logic a,b,c,d;
+    logic shift_in;
+
+    always_ff @(posedge clk) begin
+        if (rst) begin
+            shift_reg <= SEED_VALUE;
+            // rand_bit <= SEED_VALUE[5]^SEED_VALUE[3]^SEED_VALUE[2]^SEED_VALUE[0];
+        end else if (en) begin
+            // rand_bit <= shift_reg[5]^shift_reg[3]^shift_reg[2]^shift_reg[0];
+            // rand_bit <= temp_rand_bit;
+            rand_bit <= shift_reg[0];
+            shift_reg <= {shift_in, shift_reg[15:1]};
+        end else begin
+            // rand_bit <= shift_reg[5]^shift_reg[3]^shift_reg[2]^shift_reg[0];
+            // rand_bit <= temp_rand_bit;
+        end
+    end
+
+    always_comb begin
+        a = shift_reg[5];
+        b = shift_reg[3];
+        c = shift_reg[2];
+        d = shift_reg[0];
+
+        shift_in = a^b^c^d;
+    end
 
 endmodule : lfsr
